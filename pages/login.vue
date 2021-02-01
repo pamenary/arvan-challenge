fe7f63793f5d43214d6961cf29c4c86b193b6792
ali pamenary <pamenary@gmail.com>
2021-02-01 15:51:01 +0100

    add api call login

--- pages/login.vue
+++ pages/login.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="page-login d-flex justify-content-center align-items-center">
-    <LoginForm :loading="true" />
+    <LoginForm :is-loading="isLoading" @submit="onSubmit" />
   </div>
 </template>
 
 <script>
 export default {
   layout: 'blank',
+  middleware: 'notAuthenticated',
+  data() {
+    return {
+      isLoading: false,
+    }
+  },
+  methods: {
+    async onSubmit(form) {
+      try {
+        this.isLoading = true
+        const { data } = await this.$axios.post('/users/login', {
+          user: {
+            ...form,
+          },
+        })
+        this.isLoading = false
+        localStorage.setItem('user', JSON.stringify(data.user))
+        this.$store.dispatch('auth/setUser', data.user)
+        this.$router.push('/')
+      } catch (error) {
+        this.isLoading = false
+      }
+    },
+  },
 }
 </script>
 
